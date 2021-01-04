eadd5133763b274662248e8f068d752104fcd432
kaboumk <kaboumk@gmail.com>
2021-01-04 20:48:14 +0400

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  /* return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Puna",
-      animal: "dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Milouzze",
-      animal: "dog",
-      breed: "Dalmatien",
-    }),
-    React.createElement(Pet, {
-      name: "Fugazi",
-      animal: "dog",
-      breed: "Royal Bourbon",
-    }),
-  ]); */
   return (
     <div>
+      <SearchParams />
       <h1 id="something-important">Adopt Me!</h1>
       <Pet name="Luna" animal="Dog" breed="Japanese" />
       <Pet name="Milouze" animal="Dog" breed="Dalamata" />
