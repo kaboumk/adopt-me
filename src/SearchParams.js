fb8dcd6e8a4255309f14b649a1f970921786f549
kaboumk <kaboumk@gmail.com>
2021-01-04 21:52:58 +0400

    fix

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  const [location, setLocation] = useState("Where are you going?");
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
-          Location
-          <input id="location" value={location} placeholder="Location" />
+          Location: {location}
+          <input
+            id="location"
+            value={location}
+            placeholder="location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
         </label>
         <button>Submit</button>
       </form>
