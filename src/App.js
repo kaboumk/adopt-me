ca4abf9df9e141fd719867cd234e3d3669f44d2b
kaboumk <kaboumk@gmail.com>
2021-01-04 21:25:32 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
 import SearchParams from "./SearchParams";
-import Toto from "./Toto";
 
 const App = () => {
   return (
